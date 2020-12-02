07c0cfd0bb9d66cacd584f5d612d89cce0cd02e4
Cole Moore <Cartisim@icloud.com>
2020-12-02 13:14:32 -0800

    Changed library name

--- Package.swift
+++ Package.swift
@@ -12,8 +12,8 @@ let package = Package(
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "LibSignalProtocolSwift",
-            targets: ["LibSignalProtocolSwift"]),
+            name: "SignalProtocol",
+            targets: ["SignalProtocol"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
